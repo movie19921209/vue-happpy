795f036f98f7776945e7891f46bddb6f17bd363b
王庆元 <wangqingyuan@xiaomi.com>
2021-06-07 00:28:49 +0800

    下一题和提交可以互相点击切换
    Signed-off-by: 王庆元 <wangqingyuan@xiaomi.com>

--- src/components/itemcontainer.vue
+++ src/components/itemcontainer.vue
@@ -3,6 +3,9 @@
 		<span v-if="fatherComponent == 'home'">在home下</span>
 		<span v-if="fatherComponent == 'item'">在item组件下</span>
 		<h1>this is itemconstainer!</h1>
+		
+		<span class="next_item button_style" @click="nextItem" v-if="itemNum == 0"></span>
+		<span class="submit_item button_style" @click="submitAnswer" v-else></span>
 	</section>
 </template>
 
@@ -11,9 +14,44 @@
 export default {
 	name:'itemcontainer',
 	props:['fatherComponent'],
+	data() {
+		return {
+			itemNum: 0
+		}
+	},
+	created() {
+		console.log("itemNum = " + this.itemNum);
+	},
+	methods: {
+		nextItem() {
+			alert("click....");
+			this.itemNum ++;
+		},
+		submitAnswer() {
+			alert("submit answer...");
+			this.itemNum = 0;
+		}
+	}
 }
 	
 </script>
 
 <style>
+	.button_style{
+		display: block;
+		height: 2.1rem;
+		width: 4.35rem;
+		background-size: 100% 100%;
+		position: absolute;
+		top: 16.5rem;
+		left: 50%;
+		margin-left: -2.4rem;
+		background-repeat: no-repeat;
+	}
+	.next_item{
+		background-image: url(../images/2-2.png);
+	}
+	.submit_item{
+		background-image: url(../images/3-1.png);
+	}
 </style>
