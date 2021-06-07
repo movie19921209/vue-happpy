eea3278af479ec97b4dfab8aa4aae7356ba14e79
王庆元 <wangqingyuan@xiaomi.com>
2021-06-07 16:40:39 +0800

    列举答案item，暂未完全实现
    Signed-off-by: 王庆元 <wangqingyuan@xiaomi.com>

--- src/components/itemcontainer.vue
+++ src/components/itemcontainer.vue
@@ -1,8 +1,8 @@
 <template>
 	<section>
 		<header>
-			<span v-if="fatherComponent == 'home'">在home下</span>
-			<span v-if="fatherComponent == 'item'">在item组件下</span>
+			<span v-if="fatherComponent == 'home'">在home下level =  {{level}}</span>
+			<span v-if="fatherComponent == 'item'">在item组件下题目 = {{itemNum}} </span>
 			<h1>this is itemconstainer!</h1>
 		</header>
 
@@ -12,6 +12,17 @@
 		</div>
 		
 		<div v-if="fatherComponent == 'item'">
+			<div class="item_back item_container_style">
+				<div class="item_list_container" v-if="itemDetail.length > 0">
+					<header class="item_title">{{itemDetail[itemNum-1].topic_name}}</header>
+					<ul>
+						<li v-for="(item,index) in itemDetail[itemNum-1].topic_answer" class="item_list">
+							<span class="option_style">{{index}}</span>
+						</li>
+						
+					</ul>
+				</div>
+			</div>			
 			<span class="next_item button_style" @click="nextItem" v-if="itemNum == 0"></span>
 			<span class="submit_item button_style" @click="submitAnswer" v-else></span>
 		</div>
@@ -19,18 +30,22 @@
 </template>
 
 <script>
-
+import {mapState} from 'vuex'
 export default {
 	name:'itemcontainer',
 	props:['fatherComponent'],
 	data() {
 		return {
-			itemNum: 3
 		}
 	},
 	created() {
 		console.log("itemNum = " + this.itemNum);
 	},
+	computed: mapState ([
+		'itemNum',
+		'level',
+		'itemDetail',
+	]),
 	methods: {
 		nextItem() {
 			alert("click....");
@@ -50,6 +65,11 @@ export default {
 		background-image: url(../images/1-2.png);
 		background-size: 13.142rem 100%;
 		background-position: right center;
+	}
+	.item_back{
+		background-image: url(../images/2-1.png);
+		background-size: 100% 100%;
+		color: chocolate;
 	}
 	.item_container_style{
 		height: 11.625rem;
@@ -73,10 +93,50 @@ export default {
 		margin-left: -2.4rem;
 		background-repeat: no-repeat;
 	}
+	.item_list_container{
+		position: absolute;
+		height: 7.0rem;
+		width: 8.0rem;
+		top: 2.4rem;
+		left: 3rem;
+	-webkit-font-smoothing: antialiased;
+	}
 	.next_item{
 		background-image: url(../images/2-2.png);
 	}
 	.submit_item{
 		background-image: url(../images/3-1.png);
+	}
+	.item_list{
+		font-size: 0;
+		margin-top: 0.4rem;
+		width: 10rem;
+		span{
+			display: inline-block;
+			font-size: 0.6rem;
+			color: #00e;
+			vertical-align: middle;
+		}
+		.option_style{
+			height: 0.725rem;
+			width: 0.725rem;
+			border: 1px solid #fff;
+			border-radius: 50%;
+			line-height: 0.725rem;
+			text-align: center;
+			margin-right: 0.3rem;
+			font-size: 0.5rem;
+			font-family: 'Arial';
+			color: #D2691E;
+		}
+		.has_choosed{
+			background-color: #ffd400;
+			color: #575757;
+			border-color: #ffd400;
+		}
+		.option_detail{
+			width: 7.5rem;
+			padding-top: 0.11rem;
+		}
 	}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,53 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+	level: '第一周',
+	itemNum: 1,
+	itemDetail: [
+		{
+			"topic_name": "题目一",
+			"topic_answer": [
+				{
+					"topic_answer_id": 1,
+					"answer_name": "答案AAA"
+				},
+				{
+					"topic_answer_id": 2,
+					"answer_name": "答案BBB"
+				},
+				{
+					"topic_answer_id": 3,
+					"answer_name": "答案CCC"
+				},
+				{
+					"topic_answer_id": 4,
+					"answer_name": "答案DDD"
+				},
+			]
+		},
+		{
+			"topic_name": "题目二",
+			"topic_answer": [
+				{
+					"topic_answer_id": 1,
+					"answer_name": "答案111"
+				},
+				{
+					"topic_answer_id": 2,
+					"answer_name": "答案222"
+				},
+				{
+					"topic_answer_id": 3,
+					"answer_name": "答案333"
+				},
+				{
+					"topic_answer_id": 4,
+					"answer_name": "答案444"
+				},
+			]
+		},
+		
+	]
   },
   mutations: {
   },
